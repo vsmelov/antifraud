40961a921b400e392c7fd5ef6bca8d98908aa82c
Vladimir Smelov <vladimirfol@gmail.com>
2017-03-02 02:50:25 +0300

    mark

--- cy/run3.py
+++ cy/run3.py
@@ -12,18 +12,18 @@ if 0:
 
 if 1:
     setup = """
-import random
-import make_set_cython
-import make_set_python
-A = bytes([random.randint(0, 255) for i in range(3*10000)])
-B = bytes([random.randint(0, 255) for i in range(3*10000)])
-    """
+    import random
+    import make_set_cython
+    import make_set_python
+    A = [bytes([random.randint(0, 255) for i in range(3*10000)]) for j in range(10)]
+    B = [bytes([random.randint(0, 255) for i in range(3*10000)]) for j in range(10)]
+        """
 
-    code_c = """make_set_cython.detect_users(A, B)"""
-    code_c2 = """make_set_cython.detect_users2(A, B)"""
-    code_p = """make_set_python.detect_users(A, B)"""
-    code_p2 = """make_set_python.detect_users2(A, B)"""
-    number = 10
+    code_c = """make_set_cython.detect_users(A[random.randint(0, 9)], B[random.randint(0, 9)])"""
+    code_c2 = """make_set_cython.detect_users2(A[random.randint(0, 9)], B[random.randint(0, 9)])"""
+    code_p = """make_set_python.detect_users(A[random.randint(0, 9)], B[random.randint(0, 9)])"""
+    code_p2 = """make_set_python.detect_users2(A[random.randint(0, 9)], B[random.randint(0, 9)])"""
+    number = 100
 
     t_c = timeit.timeit(code_c, number=number, setup=setup) / number
     t_c2 = timeit.timeit(code_c2, number=number, setup=setup) / number
