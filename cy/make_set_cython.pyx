0009badc971b667d521fcdeb03d2958562f5e789
Vladimir Smelov <vladimirfol@gmail.com>
2017-03-02 02:48:05 +0300

    good result

--- cy/make_set_cython.pyx
+++ cy/make_set_cython.pyx
@@ -1,5 +1,4 @@
 # distutils: language = c++
-# distutils: define_macros=CYTHON_TRACE=1
 # cython: boundscheck=False
 # cython: wraparound=False
 # cython: initializedcheck=False
@@ -8,30 +7,36 @@
 # cython: profile=True
 # cython: linetrace=True
 # cython: binding=True
+# distutils: define_macros=CYTHON_TRACE=1
+
+
 
 from libcpp.string cimport string
 from unordered_set cimport unordered_set
+# from bytes2subnet cimport bytes2subnet
+
+cdef inline int bytes2subnet(char* buf, int i):
+    return (256*buf[i] + buf[i+1])*256 + buf[i+2]
+
+from libc.stdint cimport uintptr_t
 
 from cython.operator cimport dereference as deref, preincrement as inc
 from cpython.buffer cimport PyBUF_SIMPLE, PyBUF_WRITABLE, \
     PyObject_CheckBuffer, PyObject_GetBuffer, PyBuffer_Release
 
 
-cdef unordered_set[int]* _bytes2set(object buf):
-    if not PyObject_CheckBuffer(buf):
+cdef unordered_set[int]* _bytes2set(object subnets_bytes):
+    if not PyObject_CheckBuffer(subnets_bytes):
         raise TypeError("argument must follow the buffer protocol")
     cdef Py_buffer view
-    PyObject_GetBuffer(buf, &view, PyBUF_SIMPLE)
+    PyObject_GetBuffer(subnets_bytes, &view, PyBUF_SIMPLE)
     cdef int i = 0
-    cdef int subnet
     cdef unordered_set[int]* xxset = new unordered_set[int]()
     xxset.reserve(<size_t> view.len / 3)
+    cdef char* buf = <char*> view.buf
     try:
         for i in range(0, view.len, 3):
-            subnet = 256*256*(<char*>view.buf)[i]
-            subnet += 256*(<char*>view.buf)[i+1]
-            subnet += (<char*>view.buf)[i+2]
-            xxset.insert(subnet)
+            xxset.insert(bytes2subnet(buf, i))
     finally:
         PyBuffer_Release(&view)
     return xxset
@@ -39,30 +44,17 @@ cdef unordered_set[int]* _bytes2set(object buf):
 def bytes2set(object buf):
     _bytes2set(buf)
 
-def detect_users(object nets_bytes_1, object nets_bytes_2):
-    cdef unordered_set[int]* set1 = _bytes2set(nets_bytes_1)
-    cdef unordered_set[int]* set2 = _bytes2set(nets_bytes_2)
-    cdef int n_same = 0
-    for it in deref(set1):
-        if set2.find(it) != set2.end():
-            n_same += 1
-            if n_same >= 2:
-                return True
-    return False
-
-def detect_users2(object nets_bytes_1, object nets_bytes_2):
-    cdef unordered_set[int]* set1 = _bytes2set(nets_bytes_1)
+def detect_users(object user1_bytes, object user2_bytes):
+    """ Проверяет двух пользователей """
+    cdef unordered_set[int]* user1_set = _bytes2set(user1_bytes)
     cdef Py_buffer view
-    PyObject_GetBuffer(nets_bytes_2, &view, PyBUF_SIMPLE)
+    PyObject_GetBuffer(user2_bytes, &view, PyBUF_SIMPLE)
+    cdef char* buf = <char*> view.buf
     cdef int n_same = 0
     cdef int i
-    cdef subnet
     try:
         for i in range(0, view.len, 3):
-            subnet = 256*256*(<char*>view.buf)[i]
-            subnet += 256*(<char*>view.buf)[i+1]
-            subnet += (<char*>view.buf)[i+2]
-            if set1.find(subnet) != set1.end():
+            if user1_set.find(bytes2subnet(buf, i)) != user1_set.end():
                 n_same += 1
                 if n_same >= 2:
                     return True
--- cy/run3.py
+++ cy/run3.py
@@ -15,25 +15,23 @@ if 1:
 import random
 import make_set_cython
 import make_set_python
-A = bytes([random.randint(0, 255) for i in range(3*10000)])
-B = bytes([random.randint(0, 255) for i in range(3*10000)])
+A = [bytes([random.randint(0, 255) for i in range(3*10000)]) for j in range(10)]
+B = [bytes([random.randint(0, 255) for i in range(3*10000)]) for j in range(10)]
     """
 
-    code_c = """make_set_cython.detect_users(A, B)"""
-    code_c2 = """make_set_cython.detect_users2(A, B)"""
+    code_c = """make_set_cython.detect_users(A[random.randint(0, 9)], B[random.randint(0, 9)])"""
     code_p = """make_set_python.detect_users(A, B)"""
     code_p2 = """make_set_python.detect_users2(A, B)"""
     number = 100
 
     t_c = timeit.timeit(code_c, number=number, setup=setup) / number
-    t_c2 = timeit.timeit(code_c2, number=number, setup=setup) / number
-    t_p = timeit.timeit(code_p, number=number, setup=setup) / number
-    t_p2 = timeit.timeit(code_p2, number=number, setup=setup) / number
+    # t_p = timeit.timeit(code_p, number=number, setup=setup) / number
+    # t_p2 = timeit.timeit(code_p2, number=number, setup=setup) / number
 
     print("cython: {}".format(t_c))
-    print("cython2: {}".format(t_c2))
-    print("python: {}".format(t_p))
-    print("python2: {}".format(t_p2))
+    # print("cython2: {}".format(t_c2))
+    # print("python: {}".format(t_p))
+    # print("python2: {}".format(t_p2))
 
 
 if 1:
@@ -43,8 +41,4 @@ if 1:
 
     profile = line_profiler.LineProfiler(make_set_cython.detect_users)
     profile.runcall(make_set_cython.detect_users, A, B)
-    profile.print_stats()
-
-    profile = line_profiler.LineProfiler(make_set_cython.detect_users2)
-    profile.runcall(make_set_cython.detect_users2, A, B)
     profile.print_stats()
